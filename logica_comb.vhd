ec8a0b3bcf3e70a62db854cf6ff2d8764a0c8987
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2025-11-30 21:09:26 -0300

    Update logica_comb.vhd

--- logica_comb.vhd
+++ logica_comb.vhd
@@ -1,1 +1,11 @@
+library IEEE;
+use IEEE.Std_Logic_1164.all;
+use ieee.std_logic_unsigned.all; 
+use ieee.std_logic_arith.all;
 
+entity logica_comb is
+port(X, Bonus_reg: in std_logic_vector(3 downto 0);
+	 Sel: in std_logic_vector(1 downto 0);
+	 RESULT: out std_logic_vector(7 downto 0)
+    );
+end logica_comb;
