64a5fd8dfb7d77edd9e6e2155d21f67fe1f8cd81
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2025-12-02 19:24:00 -0300

    Update reg_15bits.vhd

--- reg_15bits.vhd
+++ reg_15bits.vhd
@@ -1,27 +1,27 @@
-library IEEE; 
-use IEEE.Std_Logic_1164.all; 
-use IEEE.std_logic_unsigned.all; 
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
 entity reg_15bits is 
-port (CLK:   in  std_logic;
-	  RST:   in  std_logic;
-	  N:   in  std_logic_vector(14 downto 0);
-      S: out std_logic_vector(14 downto 0)
-      ); 
-end reg_15bits; 
-
-
-architecture behv of reg_15bits is 
-
-begin
-
-	process (CLK, N)
-	begin
-		if (RST = '0') then
-			S <= "000000000000000";
-		elsif rising_edge(CLK) then
-			S <= N;
-		end if;
-	end process;
-
+port(
+	CLK, RST, enable: in std_logic;
+	D: in std_logic_vector(14 downto 0);
+	Q: out std_logic_vector(14 downto 0)
+    );
+end reg_15bits;
+        
+architecture behv of reg_15bits is
+    begin
+    process(CLK,RST)
+    begin
+        if(enable = '1') then
+            if(RST = '1') then
+                Q <= "000000000000000";
+            elsif(CLK'event AND CLK = '1') then
+    				Q <= D;		
+            end if;
+        end if;
+    end process;
+	 
 end behv;
