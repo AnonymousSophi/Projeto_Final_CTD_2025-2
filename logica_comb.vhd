968e3cb2869a980a7ca6f8be4e4c0ea6d92905de
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2025-12-02 10:39:12 -0300

    Update logica_comb.vhd
    Final version

--- logica_comb.vhd
+++ logica_comb.vhd
@@ -1,11 +1,26 @@
-library IEEE;
-use IEEE.Std_Logic_1164.all;
-use ieee.std_logic_unsigned.all; 
-use ieee.std_logic_arith.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
 
-entity logica_comb is
-port(X, Bonus_reg: in std_logic_vector(3 downto 0);
-	 Sel: in std_logic_vector(1 downto 0);
-	 RESULT: out std_logic_vector(7 downto 0)
-    );
+entity logica_comb is port(
+
+X, Bonus_reg: in std_logic_vector(3 downto 0);
+nivel: in std_logic_vector(1 downto 0);        -- Sel
+RESULT: out std_logic_vector(7 downto 0)
+);
 end logica_comb;
+
+architecture arc_logica of logica_comb is
+signal bonus_div: std_logic_vector(2 downto 0);
+signal rodadas_div: std_logic_vector(1 downto 0);
+
+begin
+
+rodadas_div <= X (3 downto 2);
+
+bonus_div <= Bonus_reg(3 downto 1);
+
+RESULT <= '0' & nivel & bonus_div & rodadas_div;
+
+
+end arc_logica;
